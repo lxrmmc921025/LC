dd9606ed6b1d1c72f9b60e2bb81f001d40cc72d8
xy <zxbjl130326@gmail.com>
2019-09-23 22:13:36 -0700

    No.303  51ms,76.94%; 40MB,100%

--- array/RangeSumQuery.java
+++ array/RangeSumQuery.java
@@ -1,18 +1,18 @@
 package array;
 
 /**
- * Created by xiaoyu on 9/23/2019.
+ * Created by az on 9/23/2019.
  */
 public class RangeSumQuery {
-        int[] count;
-        public RangeSumQuery(int[] nums) {
-            count = new int[nums.length];
-            for (int i = 0; i < nums.length; i++) {
-                //[!!] corner case; i == 0
-                count[i] = i == 0 ? nums[0]: count[i-1] + nums[i];
-            }
+    int[] count;
+    public RangeSumQuery(int[] nums) {
+        count = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            //[!!] corner case; i == 0
+            count[i] = i == 0 ? nums[0]: count[i-1] + nums[i];
         }
-        public int sumRange(int i, int j) {
-            return i == 0 ? count[j] : count[j] - count[i - 1];
-        }
+    }
+    public int sumRange(int i, int j) {
+        return i == 0 ? count[j] : count[j] - count[i - 1];
+    }
 }
